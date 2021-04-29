bfe452a7cae923e3842ab7627489f1d257b84e05
Digbijoy <dasdigbijoy@gmail.com>
2021-04-29 13:01:26 -0400

    Test Project

---
+++ index.js
@@ -0,0 +1,7 @@
+<script>
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"}
+var x = person;
+x.age = 10;
+document.getElementById("demo").innerHTML =
+person.firstName + " is " + person.age + " years old.";
+</script>
